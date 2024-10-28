7e65f8bde2ef00024ff9d91a48877b12ffa76573
dinesh <dineshc@aissel.com>
2024-10-28 12:54:28 +0530

    dsfsdf

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Listen on all network interfaces
+        port: 3000,      // Optionally set a specific port, e.g., 3000
+        hmr: {
+            host: '192.168.2.141', // You can set this to your local IP to ensure HMR works correctly across devices
+        },
+    },
 });
